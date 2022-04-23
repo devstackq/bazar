67aac690011bd1e79c8d5ac45e4a50ce867e02cd
devstackq <loginpsn@mail.ru>
2022-04-23 20:57:04 +0600

    try without docker

--- internal/admin/repository/psql/database.go
+++ internal/admin/repository/psql/database.go
@@ -2,25 +2,21 @@ package psql
 
 import (
 	"database/sql"
-	"log"
+	"fmt"
 
 	"github.com/devstackq/bazar/internal/config"
 )
 
 func InitDb(cfg config.Config) (*sql.DB, error) {
-	log.Println(cfg.DB, "db config")
 
-	psqlInfo := "postgres://rwecsnywflfryx:04d64cea97a6d17d7b4de6b5e9b0755e2ab090bbdc24b1be7e11943d618c777c@ec2-44-199-143-43.compute-1.amazonaws.com:5432/dffqhfqt0ef62l"
-
-	// psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-	// 	"password=%s dbname=%s sslmode=disable",
-	// 	cfg.DB.Host, cfg.DB.Port, cfg.DB.Username, cfg.DB.Password, cfg.DB.DBName)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		cfg.DB.Host, cfg.DB.Port, cfg.DB.Username, cfg.DB.Password, cfg.DB.DBName)
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		return nil, err
 	}
-	// db, err := sql.Open("postgres", "postgres://"+cfg.DB.Username+":"+cfg.DB.Password+"@"+cfg.DB.Host+"/"+cfg.DB.DBName+"?sslmode=disable")
 
 	if err = db.Ping(); err != nil {
 		return nil, err
--- internal/config/config.go
+++ internal/config/config.go
@@ -91,13 +91,15 @@ func GetConfig() *Config {
 			},
 		},
 
+		// psqlInfo := "postgres://rwecsnywflfryx:04d64cea97a6d17d7b4de6b5e9b0755e2ab090bbdc24b1be7e11943d618c777c@ec2-44-199-143-43.compute-1.amazonaws.com:5432/dffqhfqt0ef62l"
+
 		DB: &DBConf{
 			Dialect:  getEnvAsStr("POSTGRES_DIALECT", "pgx"),
-			Host:     getEnvAsStr("POSTGRES_URI", "postgresdb"), // 127.0.0.1 ?  postgresdb - for compose
+			Host:     getEnvAsStr("POSTGRES_URI", "ec2-44-199-143-43.compute-1.amazonaws.com"), // 127.0.0.1 ?  postgresdb - for compose
 			Port:     getEnvAsInt("POSTGRES_PORT", 5432),
-			Username: getEnvAsStr("POSTGRES_USER", "postgres"),
-			Password: getEnvAsStr("POSTGRES_PASSWORD", "postgres"),
-			DBName:   getEnvAsStr("POSTGRES_DB", "testdb"),
+			Username: getEnvAsStr("POSTGRES_USER", "rwecsnywflfryx"),
+			Password: getEnvAsStr("POSTGRES_PASSWORD", "04d64cea97a6d17d7b4de6b5e9b0755e2ab090bbdc24b1be7e11943d618c777c"),
+			DBName:   getEnvAsStr("POSTGRES_DB", "dffqhfqt0ef62l"),
 		},
 	}
 }
