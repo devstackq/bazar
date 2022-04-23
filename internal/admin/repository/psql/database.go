ca96c59a608343b6fae8033a60f3dbf756e06df0
devstackq <loginpsn@mail.ru>
2022-04-23 20:26:58 +0600

    db creds

--- internal/admin/repository/psql/database.go
+++ internal/admin/repository/psql/database.go
@@ -2,7 +2,6 @@ package psql
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 
 	"github.com/devstackq/bazar/internal/config"
@@ -13,9 +12,11 @@ func InitDb(cfg config.Config) (*sql.DB, error) {
 
 	// psqlInfo := "host=postgresdb dbname=testdb user=postgres password=postgres port=5432"
 
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		cfg.DB.Host, cfg.DB.Port, cfg.DB.Username, cfg.DB.Password, cfg.DB.DBName)
+	psqlInfo := "postgres://rwecsnywflfryx:04d64cea97a6d17d7b4de6b5e9b0755e2ab090bbdc24b1be7e11943d618c777c@ec2-44-199-143-43.compute-1.amazonaws.com:5432/dffqhfqt0ef62l"
+
+	// psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+	// 	"password=%s dbname=%s sslmode=disable",
+	// 	cfg.DB.Host, cfg.DB.Port, cfg.DB.Username, cfg.DB.Password, cfg.DB.DBName)
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
