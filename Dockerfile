8df8b82f2b931db3da19c845fe97e74074f490eb
devstackq <loginpsn@mail.ru>
2022-04-24 11:19:51 +0600

    change db creds

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,7 @@ RUN go mod download
 COPY . .
 EXPOSE 6969
 
-ENV  POSTGRES_USER=rwecsnywflfryx
+ENV  POSTGRES_USER=jdukmdmdikvyup
 ENV  POSTGRES_PASSWORD=92b83e192256d2b9c4d4173dfb66bedf264c78bd4a602a8972d987ac501f2cc8
 ENV  POSTGRES_URI=ec2-99-80-170-190.eu-west-1.compute.amazonaws.com
 ENV  POSTGRES_PORT=5432
--- internal/admin/repository/psql/database.go
+++ internal/admin/repository/psql/database.go
@@ -2,6 +2,7 @@ package psql
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	"github.com/devstackq/bazar/internal/config"
@@ -9,11 +10,10 @@ import (
 
 func InitDb(cfg config.Config) (*sql.DB, error) {
 
-	// psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-	// 	"password=%s dbname=%s sslmode=require",
-	// 	cfg.DB.Host, cfg.DB.Port, cfg.DB.Username, cfg.DB.Password, cfg.DB.DBName)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=require",
+		cfg.DB.Host, cfg.DB.Port, cfg.DB.Username, cfg.DB.Password, cfg.DB.DBName)
 
-	psqlInfo := "postgres://jdukmdmdikvyup:92b83e192256d2b9c4d4173dfb66bedf264c78bd4a602a8972d987ac501f2cc8@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/d448svjcam10be"
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		return nil, err
